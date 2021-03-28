1842b9009787fb4d9e971508a027364713e102fb
yusoki2704 <71821574+yusoki2704@users.noreply.github.com>
2021-03-29 08:37:53 +0900

    const、letなどの変数宣言

---
+++ src/index.js
@@ -0,0 +1,48 @@
+/**
+ * const、letなどの変数宣言
+ *
+ */
+// var val1 = "var変数";
+// console.log(val1);
+
+// // var変数は上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// // var変数は再宣言可能
+// var val1 = "var変数を再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// // letは上書きが可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// // letは再宣言不可能
+// let val2 = "let変数を再宣言";
+
+// const val3 = "const変数"
+// console.log(val3);
+
+// //const変数は上書き不可
+// // val3 = "const変数を上書き"
+
+// const val3 = "const変数を再宣言";
+
+// // constで定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//   name: "ゆーすけ",
+//   age: 28,
+// };
+// console.log(val4);
+// val4.name = "Yusuke";
+// val4.address = "Fukushima";
+// console.log(val4);
+
+// // constで定義した配列はプロパティの変更が可能
+// const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
