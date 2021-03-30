37ef066facb637951052da1fbdd694e6cfff22bd
yusoki2704 <71821574+yusoki2704@users.noreply.github.com>
2021-03-30 18:54:26 +0900

    テンプレート文字列

--- src/index.js
+++ src/index.js
@@ -46,3 +46,19 @@
 // val5[0] = "bird";
 // val5.push("monkey");
 // console.log(val5);
+
+/**
+ * テンプレート文字列
+ */
+
+const name = "ゆーすけ";
+const age = 28;
+//「私の名前はゆーすけです。年齢は28歳です。」
+
+// 従来の方法
+const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+console.log(message1);
+
+// テンプレート文字列を用いた方法
+const message2 = `私の名前は${name}です。年齢は${age}です。`;
+console.log(message2);
