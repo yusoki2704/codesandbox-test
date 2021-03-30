283e87b6c48c391dfa7cd4fa0a313b8834fe58c1
yusoki2704 <71821574+yusoki2704@users.noreply.github.com>
2021-03-30 19:02:47 +0900

    アロー関数の追加

--- src/index.js
+++ src/index.js
@@ -47,18 +47,43 @@
 // val5.push("monkey");
 // console.log(val5);
 
-/**
- * テンプレート文字列
- */
+// /**
+//  * テンプレート文字列
+//  */
 
-const name = "ゆーすけ";
-const age = 28;
-//「私の名前はゆーすけです。年齢は28歳です。」
+// const name = "ゆーすけ";
+// const age = 28;
+// //「私の名前はゆーすけです。年齢は28歳です。」
 
-// 従来の方法
-const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
-console.log(message1);
+// // 従来の方法
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+// console.log(message1);
 
-// テンプレート文字列を用いた方法
-const message2 = `私の名前は${name}です。年齢は${age}です。`;
-console.log(message2);
+// // テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+// /**
+//  * アロー関数
+//  */
+// // 従来の関数
+// // function func1(str) {
+// //   return str;
+// // }
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("func1です"));
+
+// // アロー関数
+// // const func2 = (str) => {
+// //   return str;
+// // };
+// const func2 = (str) => str;
+// console.log(func2("func2です"));
+
+// // const func3 = (num1, num2) => {
+// //   return num1 + num2;
+// // };
+// const func3 = (num1, num2) => num1 + num2;
+// console.log(func3(10, 20));
