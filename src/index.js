6503ad23719d062f0cf8fa032713b546d1bdaa1b
yusoki2704 <71821574+yusoki2704@users.noreply.github.com>
2021-03-31 19:34:36 +0900

    論理演算子の本当の意味

--- src/index.js
+++ src/index.js
@@ -204,3 +204,27 @@
 //   return num1 + num2 > 100 ? '100を超えています！': '許容範囲内です';
 // }
 // console.log(checkSum(50, 40))
+
+/**
+ * 論理演算子の本当の意味を知ろう && ||
+ */
+// const flag1 = true;
+// const flag2 = true;
+
+// if (flag1 || flag2) {
+//   console.log("1か2はtrueになります")
+// }
+
+// if (flag1 && flag2) {
+//   console.log("1も2もtrueになります")
+// }
+
+// || は左側がfalseなら右側を返す
+// const num = null;
+// const fee = num || "金額未設定です";
+// console.log(fee);
+
+// && は左側がtrueなら右側を返す
+// const num2 = null;
+// const fee2 = num2 && "何か設定されました";
+// console.log(fee2);
