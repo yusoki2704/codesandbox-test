f7a4dfb837b64273bc4aac8e7834bcb57fa4e3dd
yusoki2704 <71821574+yusoki2704@users.noreply.github.com>
2021-03-31 19:26:30 +0900

    三項演算子

--- src/index.js
+++ src/index.js
@@ -187,3 +187,20 @@
 //   }
 // });
 // console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+// ある条件 ? 条件がtrueの時 : 条件がfalseの時
+// const val1 = 1 < 0 ? 'trueです' : 'falseです';
+// console.log(val1);
+
+// const num = 1300;
+
+// const formattedNum = typeof num === 'number' ? num.toLocaleString() : '数値を入力してください';
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? '100を超えています！': '許容範囲内です';
+// }
+// console.log(checkSum(50, 40))
